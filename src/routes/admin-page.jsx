9b365451f86321404b7bdc76a976c5a7fd69d21c
IlyaChgn <chugin.ilya2004@gmail.com>
2024-07-09 23:54:09 +0300

    Added dropdown for navigation

--- src/routes/admin-page.jsx
+++ src/routes/admin-page.jsx
@@ -1,10 +1,33 @@
 import React, {useState} from 'react';
-import SetCoordinate from '../components/admin-page/set-coordinate.jsx';
-import ConnectionComponent from '../components/admin-page/connection-component.jsx';
+import {Link, Outlet} from 'react-router-dom';
 import styles from '../styles/admin-page.scss';
-import { Outlet } from 'react-router-dom';
 
 export default function AdminPage() {
+  const sections = [
+    {
+      name: 'Редактирование',
+      href: '/admin/editing',
+    },
+    {
+      name: 'Разметка',
+      href: '/admin/markup',
+    },
+    {
+      name: 'Свзяывание',
+      href: '/admin/connection',
+    },
+    {
+      name: 'Настройки полигона',
+      href: '/admin/settings',
+    },
+  ];
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className='admin-page'>
       <div className='admin-page__left-section'>
@@ -12,7 +35,22 @@ export default function AdminPage() {
         </div>
         <Outlet />
       </div>
-      <div className='admin-page__right-section'></div>
+      <div className='admin-page__right-section'>
+        <div className='routes-dropdown'>
+          <button className='routes-dropdown__btn' onClick={toggleVisibility}><span>Выберите опцию</span></button>
+          {isVisible && (
+            <div className='routes-dropdown__content'>
+              {sections.map((section, index) => (
+                <Link
+                  to={section.href}
+                  key={index}
+                  onClick={toggleVisibility}
+                >{section.name}</Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
